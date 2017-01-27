50fbc1cf38934b77311ee79ca5542df8babe7c08
sanchitrathee <sanchit2411@gmail.com>
2017-01-27 11:47:18 +0530

    demo branch

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+/**$(document).ready(function(){
+		var measure = $('.inner-footer').offset().top;
+console.log(measure);
+});
+**/
+
+// $(window).scroll(function(){
+// 	console.log($(this).scrollTop());
+// 	if($(window).scrollTop() > 763){
+// 		$('.fix_box').css("position", "static");
+// 	}
+// 	else{
+// 		$('.fix_box').css("position", "fixed");
+// 	}
+	
+// })
