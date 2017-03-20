f1b1bd275b4db66dfff26eb2fc7c21e22438afd0
thakurdivendra <dkghlaut111@gmail.com>
2017-03-20 17:39:16 +0530

    add landing page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 /* ================================================
 ----------- Venedor ---------- */
+$(function() {
+  $('nav#main-nav li > a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-50
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 (function ($) {
 	"use strict";
 	
