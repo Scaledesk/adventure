416d788e78ba51cadf025840c3170460805a9db1
sanchitrathee <sanchit2411@gmail.com>
2017-03-23 23:33:28 +0530

    8 packages text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* ================================================
 ----------- Venedor ---------- */
 $(function() {
-  $('nav#main-nav li > a').click(function() {
+  $('nav#main-nav li > a, a.scroll-to').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
